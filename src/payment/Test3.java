31566ab9dc803d00b87467f737a265645d524e73
Satishkumar17 <55325181+Satishkumar17@users.noreply.github.com>
2019-10-19 15:01:39 +0530

    Update Test3.java

--- src/payment/Test3.java
+++ src/payment/Test3.java
@@ -2,9 +2,9 @@ package payment;
 
 public class Test3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.print("Satish");
 	}
 
 }
